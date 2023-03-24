44e7ce40aa3d1cf3851c8e09e86ae1f610e0d25b
xCoder <xcoder@xCoders-iMac.local>
2023-03-24 17:43:29 +0600

    Counter Set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   return (
     <div className="App">
-
+      <Counter></Counter>
     </div>
+  )
+}
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+
+  }
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button>Increse</button>
+      <button>Decrese</button>
+    </div>
+
   )
 }
 
