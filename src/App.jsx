b4db7a3c4f4a12c4c39127269bb6490b6ff2311f
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-03-25 04:40:59 +0600

    API use

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   )
 }
 
-function Counter() {
+function ExternalUsers() {
 
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
 
-  const increaseCount = () => {
+  }, []);
 
-  }
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
-      <button>Increse</button>
-      <button>Decrese</button>
+      <h1>External Users</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
+  )
+}
 
+function User(props) {
+  return (
+    <div style={{ border: "2px solid red", margin: "5px" }}>
+      <h3>Name: {props.name}</h3>
+      <h4>E-Mail: {props.email}</h4>
+    </div>
   )
 }
 
